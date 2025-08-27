fb10dd59ac85cdae1c7fd35a343a7b4ae1100d01
mshdabiola <mshdabiola@gmail.com>
2025-08-27 11:39:38 +0100

    Feat: Add new outlined icons to KmtIcons
    This commit expands the `KmtIcons` object by adding a comprehensive set of new outlined Material Design icons.
    
    **Key Changes:**
    
    *   **Added Outlined Icons:**
        *   `Alarm`
        *   `Repeat`
        *   `RestoreFromTrash`
        *   `Edit`
        *   `ViewAgenda`
        *   `GridView`
        *   `CheckBoxOutlineBlank`
        *   `Undo` (auto-mirrored)
        *   `Redo` (auto-mirrored)
        *   `AccessTime`
        *   `Share`
        *   `ContentCopy`
        *   `Unarchive`
        *   `PushPinD` (default variant)
        *   `PushPin`
        *   `NotificationAdd`
        *   `AddBox`
        *   `ColorLens`
        *   `Clear`
        *   `PauseCircle`
        *   `PlayCircle`
        *   `ImageNotSupported`
        *   `Done`
        *   `FormatColorReset`
        *   `PhotoCamera`
        *   `Brush`
        *   `CheckBox`
        *   `Image`
        *   `KeyboardVoice`
        *   `Cancel` (maps to `Close`)
        *   `MoreVert`
        *   `Search`
        *   `Archive`
        *   `Info`
        *   `Label` (auto-mirrored)
        *   `Notification`
        *   `Link`
        *   `More` (maps to `ExpandMore`)
        *   `Less` (maps to `ExpandLess`)
    
    This provides a richer set of icons for use throughout the application's UI.

--- core/designsystem/src/commonMain/kotlin/com/mshdabiola/designsystem/drawable/KmtIcons.kt
+++ core/designsystem/src/commonMain/kotlin/com/mshdabiola/designsystem/drawable/KmtIcons.kt
@@ -16,11 +16,54 @@
 package com.mshdabiola.designsystem.drawable
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.automirrored.outlined.Label
+import androidx.compose.material.icons.automirrored.outlined.Redo
+import androidx.compose.material.icons.automirrored.outlined.Undo
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.automirrored.rounded.Chat
 import androidx.compose.material.icons.automirrored.rounded.MenuOpen
+import androidx.compose.material.icons.filled.PushPin
+import androidx.compose.material.icons.outlined.AccessTime
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.AddBox
+import androidx.compose.material.icons.outlined.Alarm
+import androidx.compose.material.icons.outlined.Archive
+import androidx.compose.material.icons.outlined.Brush
+import androidx.compose.material.icons.outlined.CheckBox
+import androidx.compose.material.icons.outlined.CheckBoxOutlineBlank
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.ColorLens
+import androidx.compose.material.icons.outlined.ContentCopy
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Done
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.material.icons.outlined.FormatColorReset
+import androidx.compose.material.icons.outlined.GridView
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Image
+import androidx.compose.material.icons.outlined.ImageNotSupported
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.KeyboardVoice
+import androidx.compose.material.icons.outlined.Link
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.NotificationAdd
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.PauseCircle
+import androidx.compose.material.icons.outlined.PhotoCamera
+import androidx.compose.material.icons.outlined.PlayCircle
+import androidx.compose.material.icons.outlined.PushPin
+import androidx.compose.material.icons.outlined.Repeat
+import androidx.compose.material.icons.outlined.Restore
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material.icons.outlined.Unarchive
+import androidx.compose.material.icons.outlined.ViewAgenda
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.BugReport
 import androidx.compose.material.icons.rounded.Check
@@ -68,4 +111,47 @@ object KmtIcons {
     val Faq = Icons.AutoMirrored.Rounded.Chat
 
     val AppIcon = com.mshdabiola.designsystem.drawable.AppIcon
+
+
+    val Alarm = Icons.Outlined.Alarm
+    val Repeat = Icons.Outlined.Repeat
+    val RestoreFromTrash = Icons.Outlined.Restore
+    val Edit = Icons.Outlined.Edit
+    val ViewAgenda = Icons.Outlined.ViewAgenda
+    val GridView = Icons.Outlined.GridView
+    val CheckBoxOutlineBlank = Icons.Outlined.CheckBoxOutlineBlank
+    val Undo = Icons.AutoMirrored.Outlined.Undo
+    val Redo = Icons.AutoMirrored.Outlined.Redo
+    val AccessTime = Icons.Outlined.AccessTime
+    val Share = Icons.Outlined.Share
+    val ContentCopy = Icons.Outlined.ContentCopy
+    val Unarchive = Icons.Outlined.Unarchive
+    val PushPinD = Icons.Default.PushPin
+    val PushPin = Icons.Outlined.PushPin
+    val NotificationAdd = Icons.Outlined.NotificationAdd
+    val AddBox = Icons.Outlined.AddBox
+    val ColorLens = Icons.Outlined.ColorLens
+    val Clear = Icons.Outlined.Clear
+    val PauseCircle = Icons.Outlined.PauseCircle
+    val PlayCircle = Icons.Outlined.PlayCircle
+    val ImageNotSupported = Icons.Outlined.ImageNotSupported
+    val Done = Icons.Outlined.Done
+    val FormatColorReset = Icons.Outlined.FormatColorReset
+    val PhotoCamera = Icons.Outlined.PhotoCamera
+    val Brush = Icons.Outlined.Brush
+    val CheckBox = Icons.Outlined.CheckBox
+    val Image = Icons.Outlined.Image
+    val KeyboardVoice = Icons.Outlined.KeyboardVoice
+    val Cancel = Icons.Outlined.Close
+    val MoreVert = Icons.Outlined.MoreVert
+    val Search = Icons.Outlined.Search
+    val Archive = Icons.Outlined.Archive
+    val Info = Icons.Outlined.Info
+    val Label = Icons.AutoMirrored.Outlined.Label
+    val Notification = Icons.Outlined.Notifications
+    val Link = Icons.Outlined.Link
+
+    val More = Icons.Outlined.ExpandMore
+    val Less = Icons.Outlined.ExpandLess
+
 }
