1ad7e51718ebf754f0b170ab647451d794c84f9d
mshdabiola <mshdabiola@gmail.com>
2025-09-01 00:10:13 +0100

    Refactor: Restructure `SearchState` sealed class
    This commit refactors the `SearchState` sealed class in `feature/main/src/commonMain/kotlin/com/mshdabiola/main/model/SearchState.kt`.
    
    **Key Changes:**
    
    *   **Removed `Loading` state:** The `SearchState.Loading` data object has been removed.
    *   **Introduced `Select` state:** A new data class `SearchState.Select` is added. This state holds lists for `types`, `color`, and `label` which were previously part of the `Success` state.
    *   **Modified `Success` state:**
        *   The `types`, `color`, and `label` properties have been moved to the new `Select` state.
        *   A new `isGrid` boolean property has been added to the `Success` state to indicate the layout format.
    
    This restructuring aims to better represent the different phases and configurations of the search feature.

--- feature/main/src/commonMain/kotlin/com/mshdabiola/main/model/SearchState.kt
+++ feature/main/src/commonMain/kotlin/com/mshdabiola/main/model/SearchState.kt
@@ -1,31 +1,20 @@
-/*
- * Designed and developed by 2024 mshdabiola (lawal abiola)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.mshdabiola.main.model
 
 import com.mshdabiola.model.note.NotePad
 
 sealed class SearchState {
-    data object Loading : SearchState()
 
-    data class Success(
-        val searches: List<NotePad> = emptyList(),
+    data class Select(
+
         val types: List<SearchSort.Type> = emptyList(),
         val color: List<SearchSort.Color> = emptyList(),
         val label: List<SearchSort.Label> = emptyList(),
+    ) : SearchState()
+
+    data class Success(
+        val searches: List<NotePad> = emptyList(),
+        val isGrid: Boolean = false,
         val searchSort: SearchSort? = null,
 
-    ) : SearchState()
+        ) : SearchState()
 }
